67fea732fd1912d945f4dc5aa1bea6990f4d685e
Youngjun Choi <juni2004c@gmail.com>
2023-02-18 18:05:00 +0900

    feat: 투두 기본 세팅 추가

--- App.js
+++ App.js
@@ -1,11 +1,112 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { EvilIcons } from '@expo/vector-icons';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+  Alert,
+} from 'react-native';
+import { theme } from './colors';
+
+const STORAGE_KEY = '@todos';
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState({});
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (e) => setText(e);
+
+  const saveTodos = (toSave) => {
+    return AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
+  };
+  const addTodo = async () => {
+    if (!text) {
+      return;
+    }
+    const newTodos = { ...todos, [Date.now()]: { text, working } };
+    setTodos(newTodos);
+    await saveTodos(newTodos);
+    setText('');
+  };
+  const loadTodos = async () => {
+    const prevTodos = await AsyncStorage.getItem(STORAGE_KEY);
+    setTodos(JSON.parse(prevTodos));
+    console.log(prevTodos);
+  };
+
+  const deleteTodo = async (key) => {
+    Alert.alert('Delete Todo?', 'Are you sure?', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {
+        text: 'OK',
+        onPress: async () => {
+          const newTodos = { ...todos };
+          delete newTodos[key];
+          setTodos(newTodos);
+          await saveTodos(newTodos);
+        },
+      },
+    ]);
+  };
+
+  useEffect(() => {
+    loadTodos();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{ ...styles.btnText, color: working ? 'white' : theme.grey }}
+          >
+            Work
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={travel}
+          style={{ ...styles.btnText, color: working ? 'white' : theme.grey }}
+        >
+          <Text
+            style={{ ...styles.btnText, color: working ? theme.grey : 'white' }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TextInput
+          onSubmitEditing={addTodo}
+          onChangeText={onChangeText}
+          returnKeyType='done'
+          placeholder={working ? 'Add a Todo' : 'Where do you want to go'}
+          style={styles.input}
+          value={text}
+        />
+      </View>
+      <ScrollView>
+        {Object.keys(todos).map((key) =>
+          todos[key].working === working ? (
+            <View key={key} style={styles.todo}>
+              <Text style={styles.todoText}>{todos[key].text}</Text>
+              <TouchableOpacity onPress={() => deleteTodo(key)}>
+                <EvilIcons name='trash' size={24} color='white' />
+              </TouchableOpacity>
+            </View>
+          ) : null
+        )}
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +114,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
+    paddingHorizontal: 20,
+  },
+  header: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 38,
+    fontWeight: '600',
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    marginVertical: 20,
+    fontSize: 18,
+  },
+  todo: {
+    backgroundColor: theme.grey,
+    marginBottom: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderRadius: 10,
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+  },
+  todoText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '500',
   },
 });
